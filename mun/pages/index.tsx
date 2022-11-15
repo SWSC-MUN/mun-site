3f9dc05f1bf2fbf55b34556fe421844d8766e6fa
Crox-x <saradgaihre19@gmail.com>
2022-11-15 16:17:27 +0000

    added facebook and instagram logo

--- mun/pages/index.tsx
+++ mun/pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Nav from '../components/Nav';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -12,39 +12,42 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        <Nav />
+      <Nav />
       <main className={styles.main}>
         <div className="container">
-          <div className="bg-[url('/home.jpg')] bg-center absolute w-screen h-screen blur-sm">
+          <div className="bg-[url('/home.jpg')] bg-center absolute w-screen h-screen blur-sm"></div>
 
-        </div>
-
-        <div className="bg-black bg-opacity-50 relative w-screen h-screen z-50">
-          <div className="flex flex-col justify-center items-center h-full">
-            <h1 className="text-5xl font-bold text-center text-white">
-              Welcome to the  SWSC-MUN MMXXII
-            </h1>
-            <h2 className="text-2xl font-bold text-center text-white">
-               MMXXII SWSC-MUN will be held on 21th to 23th of December, 2022
-            </h2>
-            <h2 className="text-2xl font-bold text-center text-white">
-              Registration is now open!
-            </h2>
-            <a href="https://forms.gle/6Z7Z7Z7Z7Z7Z7Z7Z7" target="_blank" rel ="noreferrer" className="bg-orange-600 text-white px-5 py-2 rounded-md text-sm font-medium">
-              Register Now
-            </a>
+          <div className="bg-black bg-opacity-50 relative w-screen h-screen z-50">
+            <div className="flex flex-col justify-center items-center h-full">
+              <h1 className="text-5xl font-bold text-center text-white">
+                Welcome to the SWSC-MUN MMXXII
+              </h1>
+              <h2 className="text-2xl font-bold text-center text-white">
+                MMXXII SWSC-MUN will be held on 21th to 23th of December, 2022
+              </h2>
+              <h2 className="text-2xl font-bold text-center text-white">
+                Registration is now open!
+              </h2>
+              <a
+                href="https://forms.gle/6Z7Z7Z7Z7Z7Z7Z7Z7"
+                target="_blank"
+                rel="noreferrer"
+                className="bg-orange-600 hover:bg-orange-400 text-white px-5 py-2 rounded-md text-m font-medium m-10"
+              >
+                Register Now
+              </a>
+            </div>
           </div>
         </div>
-        </div>
-        
 
-       { // Welcome to SWSC-MUN 
-}       
-       
- {
+        {
+          // Welcome to SWSC-MUN
+        }
+
+        {
           // Message from secretary general
         }
-        <div className="bg-black -screen border-b-4 border-white grid-cols-2 p-10 pt-20">
+        <div className="bg-black -screen border-b-4 border-white grid grid-cols-2 p-10 pt-20">
           <div className="flex flex-col items-center h-full">
             <h1 className="text-5xl font-bold text-center text-white">
               Message from the Secretary General
@@ -53,7 +56,11 @@ export default function Home() {
               Dear Delegates,
             </h2>
             <h2 className="text-2xl font-bold text-center text-white">
-              It is my great pleasure to welcome you to the 2022 SWSC-MUN. I am honored to serve as the Secretary General of this conference. I am confident that this conference will be a great success and will be a memorable experience for all of us. I am looking forward to meeting you all in December.
+              It is my great pleasure to welcome you to the 2022 SWSC-MUN. I am
+              honored to serve as the Secretary General of this conference. I am
+              confident that this conference will be a great success and will be
+              a memorable experience for all of us. I am looking forward to
+              meeting you all in December.
             </h2>
             <h2 className="text-2xl font-bold text-center text-white">
               Sincerely,
@@ -62,36 +69,60 @@ export default function Home() {
               Secretary General
             </h2>
           </div>
+          <div></div>
         </div>
         {
           // About
         }
         <div className="bg-black w-screen h-screen border-b-4 border-white">
           <div className="flex flex-col justify-center items-center h-full">
-            <h1 className="text-5xl font-bold text-center text-white">
-              About
-            </h1>
+            <h1 className="text-5xl font-bold text-center text-white">About</h1>
             <h2 className="text-2xl font-bold text-center text-white">
-              SWSC-MUN is a Model United Nations conference organized by the students of the School of World Studies and Culture, Shanghai International Studies University. The conference is held annually in December. The conference is a great opportunity for students to learn about the United Nations and international relations. The conference is also a great opportunity for students to meet new friends from all over the world.
+              SWSC-MUN is a Model United Nations conference organized by the
+              students of the School of World Studies and Culture, Shanghai
+              International Studies University. The conference is held annually
+              in December. The conference is a great opportunity for students to
+              learn about the United Nations and international relations. The
+              conference is also a great opportunity for students to meet new
+              friends from all over the world.
             </h2>
           </div>
         </div>
         {
           // Contact
         }
-        <div className="bg-white bg-opacity-50 absolute w-screen h-screen">
-          <div className="flex flex-col justify-center items-center h-full">
-            <h1 className="text-5xl font-bold text-center text-white">
-              Contact
-            </h1>
+        <div className="bg-blue-900 bg-opacity-50 absolute w-screen">
+          <div className="flex justify-center items-center h-full">
+            <div className="flex justify-center items-center">
+              <h1 className="text-5xl font-bold text-center text-white">
+                Contact Us
+              </h1>
+            <div>
+              <a href="https://www.facebook.com/SWSCMUN">
+                <Image
+                  src="/facebook.svg"
+                  alt="facebook"
+                  width={100}
+                  height={100}
+                />
+              </a>
+              <a href="https://www.instagram.com/SWSCMUN_">
+                <Image 
+                  src="/instagram.svg"
+                  alt="instagram"
+                  width={100}
+                  height={100}
+                />
+                </a>
+            </div>
+            </div>
             <h2 className="text-2xl font-bold text-center text-white">
               Email:
             </h2>
-            <h2 className="text-2xl font-bold text-center text-white">
-              </h2>
+            <h2 className="text-2xl font-bold text-center text-white"></h2>
           </div>
-          </div>
+        </div>
       </main>
     </div>
-  )
+  );
 }
