a42111d88397d2cdf5205ce15713c58809386ff7
Crox-x <saradgaihre19@gmail.com>
2022-12-07 15:24:26 +0000

    defined api data

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,21 @@
 mod mongo;
 use mongo::{DB, User};
 
-use actix_web::{get, App, HttpServer,web, Responder};
+use actix_web::{get, App, HttpServer,web, Responder, client::Client};
 
 
 #[get("/api/data")]
 async fn data(db: web::Data<DB>, form: web::Json<User>) -> impl Responder {
+   let mut client = Client::default();
+   let kh_verify = form.payment.token.clone();
+    let res = client.post("https://khalti.com/api/v2/payment/verify/")
+    .header ("Authorization ", "Key test secret key") // test secret key
+    .send_payload(form)
+    .await
+    .map_err(|e| e.to_string())?
+    and_then(|res| res.json::<User>().map_err(|e| e.to_string()));
+    println!("Response: {:?}", res);
+
     match db.insert_delegates(form).await {
         Ok(_) => "Success",
         Err(_) => "Failed",
