5fdb0e1871c4106109ae774420bf454258d2f7bb
Crox-x <saradgaihre19@gmail.com>
2022-12-11 16:54:47 +0000

    commit bug

--- backend/src/mongo.rs
+++ backend/src/mongo.rs
@@ -1,10 +1,10 @@
 
 use mongodb::{options::ClientOptions,Client};
-use actix_web::{web};
+use actix_web::web;
 use serde::{Deserialize, Serialize};
 pub use mongodb::bson::{doc,Bson};
 use dotenv::dotenv;
-use std::env;
+// use std::env;
 
 #[derive(Clone)]
 pub struct DB{
@@ -17,7 +17,8 @@ pub struct User{
     pub middle_name: Option<String>,
     pub email: String,
     pub school: String,
-    pub country: String,
+    pub country1: String,
+    pub country2: String,
     pub committee: String,
     pub position: String,
     pub phone: i32,
@@ -28,6 +29,7 @@ impl DB{
     pub async fn new() -> Self{
         dotenv().ok();
         // let uri = env::var("MONGO_URI").expect("MONGO_URI must be set");
+        let uri = String::new();
         let client_options = ClientOptions::parse(&uri).await.expect("Failed to parse client options");
         let client = Client::with_options(client_options).expect("Failed to initialize client");
         Self{
